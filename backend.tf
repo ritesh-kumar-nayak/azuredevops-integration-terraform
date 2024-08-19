f17b76930faa523e6e1cd41d067aa718b76fe951
RiteshKumarNayak <riteshnayakgudu49@gmail.com>
2024-08-19 22:08:58 +0530

    updated job name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfvmex-resources"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name  = "storage-rg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "storetfaccritesh"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "statefilestore"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
